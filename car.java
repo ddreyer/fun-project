71aa2b13040843c88171eaee0161cee37c47739f
Dylan Dreyer <ddreyer@berkeley.edu>
2016-01-04 19:53:59 -0800

    building a car

---
+++ car.java
@@ -0,0 +1,37 @@
+public class Car {
+    private String img;
+    private double xVelo, yVelo, x, y;
+
+    public Car(String img, double startX, double startY) {
+        this.img = img;
+        yVelo = 0;
+        xVelo = 0;
+        x = startX;
+        y = startY;
+    }
+
+    public void setXVelo(double newXVelo) {
+        xVelo = newXVelo;
+    }
+
+    public double getXVelo() {
+        return xVelo;
+    }
+
+    public void setYVelo(double newYVelo) {
+        yVelo = newYVelo;
+    }
+
+    public double getYVelo() {
+        return yVelo;
+    }
+
+    public void draw() {
+        StdDraw.picture(x,y,img);
+    }
+
+    public void update() {
+        
+    }
+}
+
